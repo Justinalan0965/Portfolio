30285492d23305e974e57cf38fea282daef1f008
Muthu Raj <mraj56965@gmail.com>
2023-09-02 22:18:51 +0530

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,62 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import c from "./tech/c.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import java from "./tech/java.png";
+import nodejs from "./tech/nodejs.png";
+import sql from "./tech/sql.png";
+import aws from "./tech/aws.png";
+import tailwind from "./tech/tailwind.png";
+import python from "./tech/python.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import chatgpt from "./chatgpt.png";
+import Pricetracker from "./Pricetracker.png";
+import GIFs from "./GIF.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  c,
+  git,
+  html,
+  javascript,
+  java,
+  nodejs,
+  sql,
+  aws,
+  tailwind,
+  python,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  chatgpt,
+  Pricetracker,
+  GIFs,
+};
